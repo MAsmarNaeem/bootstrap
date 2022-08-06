ff26bf82c4ce11c8ae347bd8b43147c58dea9493
Muhammad Asmar Naeem <110281598+MAsmarNaeem@users.noreply.github.com>
2022-08-06 12:38:18 -0700

    arithmtic operator

---
+++ project 1 bootstrap/arithmetic operator/app.ts
@@ -0,0 +1,48 @@
+
+//operators
+//Arithmetic operator
+
+//Addition
+var n1:number=10
+var n2:number=10
+var n3:number=n1+n2
+console.log("sum is : ",n3)
+
+//subtraction
+
+var firstNum=10
+var secondNum=20
+var n3=secondNum-firstNum
+console.log("subtraction is :",n3)
+
+//Multiply
+
+var firstNumb:Number=10.6
+var secondNumb:Number=4.4
+var thirdNum:Number=firstNumb*secondNumb
+console.log("multiply is :",thirdNum)
+
+//mixture
+
+var firstNum:Number=2
+var secondNum:Number=4
+var thirdNum:Number=   4+8-90+4+2/firstNum*secondNum
+console.log("Answer is :",thirdNum)
+
+
+//increment and decrement operator
+var firstNumber=3
+
+var thirdNum=firstNumber++
+console.log("third number",thirdNum)
+console.log("first Number",firstNumber)
+
+
+var firstNumber=3
+
+var thirdNum=++firstNumber
+console.log("third number"+thirdNum)
+console.log("first Number"+firstNumber)
+
+
+
